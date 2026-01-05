f720b1c4e163bf1f80369a2238507a96fcf8618d
Silvia Naim <silvia.naim@dansmultipro.com>
2026-01-05 16:10:14 +0700

    update missing import

--- src/pages/LargeListsPage.jsx
+++ src/pages/LargeListsPage.jsx
@@ -2,6 +2,7 @@ import { useState, useMemo } from 'react'
 import { Link } from 'react-router-dom'
 import './PageLayout.css'
 import PageHeader from '../components/PageHeader'
+import InfoBox from '../components/InfoBox'
 
 // Simple virtual list implementation
 function VirtualList({ items, itemHeight = 50, containerHeight = 600 }) {
